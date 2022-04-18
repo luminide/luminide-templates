6397e34579188f4bb715c8faad61e2c3e89cd6d1
Anil Thomas <anlthms@users.noreply.github.com>
2022-04-18 18:10:10 +0000

    Add template for Sorghum Identification - FGVC 9

--- pytorch-kaggle-sorghum/{{cookiecutter and 'code'}}/inference.py
+++ pytorch-kaggle-sorghum/{{cookiecutter and 'code'}}/inference.py
@@ -18,13 +18,13 @@ print(f'Running on {device}')
 def create_test_loader(conf, input_dir, class_names):
     test_aug = make_test_augmenter(conf)
 
-    image_dir = 'test_images'
+    image_dir = '{{ cookiecutter.test_image_dir }}'
     test_df = pd.DataFrame()
     image_files = sorted(glob(f'{input_dir}/{image_dir}/*.*'))
     assert len(image_files) > 0, f'No files inside {input_dir}/{image_dir}'
-    image_files = [os.path.basename(filename) for filename in image_files]
-    test_df['image'] = image_files
-    test_df['labels'] = class_names[0]
+    image_files = [os.path.basename(filename.replace('.jpg', '.png')) for filename in image_files]
+    test_df['{{ cookiecutter.image_column }}'] = image_files
+    test_df['{{ cookiecutter.label_column }}'] = class_names[0]
     test_dataset = VisionDataset(
         test_df, conf, input_dir, image_dir,
         class_names, test_aug)
@@ -45,37 +45,32 @@ def create_model(model_dir, num_classes):
 
 def test(loader, model, num_classes):
     sigmoid = nn.Sigmoid()
-    preds = np.zeros((len(loader.dataset), num_classes), dtype=np.float32)
+    preds = np.zeros(len(loader.dataset), dtype=np.float32)
     start_idx = 0
     model.eval()
     with torch.no_grad():
         for images, _ in loader:
             images = images.to(device)
             outputs = model(images)
-            # multi-label classification:
-            # set the prediction to 1 iff sigmoid(output) >= 0.5
-            pred_batch = sigmoid(outputs).round().cpu().numpy()
+            pred_batch = outputs.argmax(axis=1).cpu().numpy()
             num_rows = pred_batch.shape[0]
-            # for each example, pick the label that was predicted as most likely
-            max_inds = outputs.argmax(axis=1).cpu().numpy()
-            # this is to make sure that at least one class is predicted
-            pred_batch[range(num_rows), max_inds] = 1
             end_idx = start_idx + num_rows
             preds[start_idx:end_idx] = pred_batch
             start_idx = end_idx
     return preds
 
 def save_results(df, preds, class_names):
-    pred_names = []
-    for row in preds:
-        pred_names.append(' '.join(class_names[np.where(row)]))
-    df['labels'] = pred_names
+    pred_names = [class_names[int(pred)] for pred in preds]
+    df['{{ cookiecutter.label_column }}'] = pred_names
+    df.rename(columns={'{{ cookiecutter.image_column }}': 'filename'}, inplace=True)
     df.to_csv('submission.csv', index=False)
     print('Saved submission.csv')
 
 def run(input_dir, model_dir):
     meta_file = os.path.join(input_dir, '{{ cookiecutter.train_metadata }}')
     train_df = pd.read_csv(meta_file, dtype=str)
+    # drop invalid rows
+    train_df.drop(np.where(train_df['{{ cookiecutter.label_column }}'].isnull())[0], inplace=True)
     class_names = np.array(get_class_names(train_df))
     num_classes = len(class_names)
 
