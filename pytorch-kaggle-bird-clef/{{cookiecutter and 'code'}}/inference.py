2539b2e796a11e6cb04179a60d52f8b1a8bea402
Anil Thomas <anlthms@users.noreply.github.com>
2022-05-04 18:42:55 +0000

    Customize template for bird-clef challenge

--- pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/inference.py
+++ pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/inference.py
@@ -1,4 +1,5 @@
 import os
+import json
 from glob import glob
 import multiprocessing as mp
 import numpy as np
@@ -10,6 +11,7 @@ import torch.utils.data as data
 from util import get_class_names, make_test_augmenters
 from dataset import AudioDataset
 from models import ModelWrapper
+from prep import prep_metadata
 from config import Config
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -18,16 +20,16 @@ print(f'Running on {device}')
 def create_test_loader(conf, input_dir, class_names):
     test_audio_aug, test_image_aug = make_test_augmenters(conf)
 
-    data_dir = 'test'
+    data_dir = 'test_soundscapes'
     test_df = pd.DataFrame()
-    data_files = sorted(glob(f'{input_dir}/{data_dir}/*.*'))
+    data_files = sorted(glob(f'{input_dir}/{data_dir}/*.ogg'))
     assert len(data_files) > 0, f'No files inside {input_dir}/{data_dir}'
     data_files = [os.path.basename(filename) for filename in data_files]
-    test_df['filename'] = data_files
+    test_df['files'] = data_files
     test_df['labels'] = class_names[0]
     test_dataset = AudioDataset(
         test_df, conf, input_dir, data_dir,
-        class_names, test_audio_aug, test_image_aug)
+        class_names, test_audio_aug, test_image_aug, is_test=True)
     print(f'{len(test_dataset)} examples in test set')
     loader = data.DataLoader(
         test_dataset, batch_size=conf.batch_size, shuffle=False,
@@ -46,14 +48,8 @@ def create_model(model_dir, num_classes):
 def predict_batch(outputs, threshold):
     sigmoid = nn.Sigmoid()
     # multi-label classification:
-    # first, set prediction to 1 if probability >= threshold
-    pred_batch = sigmoid(outputs).cpu().numpy() >= threshold
-    num_rows = pred_batch.shape[0]
-    # for each example, pick the label that was predicted as most likely
-    max_inds = outputs.argmax(axis=1).cpu().numpy()
-    # this is to make sure that at least one class is predicted
-    pred_batch[range(num_rows), max_inds] = 1
-    return pred_batch
+    # set prediction to 1 if probability >= threshold
+    return sigmoid(outputs).cpu().numpy() >= threshold
 
 def test(loader, model, num_classes, threshold):
     preds = np.zeros((len(loader.dataset), num_classes), dtype=np.float32)
@@ -68,25 +64,43 @@ def test(loader, model, num_classes, threshold):
             start_idx = end_idx
     return preds
 
-def save_results(df, preds, class_names):
-    pred_names = []
-    for row in preds:
-        pred_names.append(' '.join(class_names[np.where(row)]))
-    df['labels'] = pred_names
-    df.to_csv('submission.csv', index=False)
+def save_results(input_dir, df, preds, class_names):
+    class_map = {class_names[i]: i for i in range(len(class_names))}
+    with open(f'{input_dir}/scored_birds.json') as json_file:
+        birds = json.load(json_file)
+
+    pred_idx = 0
+    row_ids = []
+    targets = []
+    for filename in df['files'].values:
+        for clip_idx in range(12):
+            end_time = 5*(clip_idx + 1)
+            clip_preds = preds[pred_idx]
+            pred_idx += 1
+            for bird in birds:
+                key = f'{filename[:-4]}_{bird}_{end_time}'
+                pred = clip_preds[class_map[bird]] > 0
+                row_ids.append(key)
+                targets.append(pred)
+    subm = pd.DataFrame()
+    subm['row_id'] = row_ids
+    subm['target'] = targets
+    subm.to_csv('submission.csv', index=False)
     print('Saved submission.csv')
 
-def run(input_dir, model_dir, threshold):
-    meta_file = os.path.join(input_dir, '{{ cookiecutter.train_metadata }}')
+def run(input_dir, model_dir, threshold=0.3):
+    meta_file = 'train.csv'
+    prep_metadata(input_dir)
     train_df = pd.read_csv(meta_file, dtype=str)
     class_names = np.array(get_class_names(train_df))
     num_classes = len(class_names)
 
     model, conf = create_model(model_dir, num_classes)
     loader, df = create_test_loader(conf, input_dir, class_names)
+    assert len(loader.dataset) == 12*df.shape[0]
     preds = test(loader, model, num_classes, threshold)
-    save_results(df, preds, class_names)
+    save_results(input_dir, df, preds, class_names)
 
 if __name__ == '__main__':
-    run('../input', './', 0.5)
+    run('../input', './')
 
