1b93ad15c030b8a438761af06087e27707b1a342
Anil Thomas <anlthms@users.noreply.github.com>
2022-05-04 22:33:19 +0000

    Add a few enhancements to the bird-clef template
    - Add a denoise transform
    - Add validation
    - Add an attention head to the audio classifier
    - Pick clips from random offsets while training
    - Change threshold to 0.2

--- pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/full.sh
+++ pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/full.sh
@@ -4,7 +4,8 @@
 # Need an additional package? Install it here via:
 #  pip3 install package-name
 
-pip3 -q install audiomentations
+pip3 install -q -r ../code/requirements.txt
+
 # Edit the line below to run your experiment (this is just an example). Note:
 #  - This script will be run from your output directory
 #  - Imported Data is accessible via the relative path ../input/
