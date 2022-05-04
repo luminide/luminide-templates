6d6b886c05cbd4936a1f0294c3809e8b66718dbc
Anil Thomas <anlthms@users.noreply.github.com>
2022-05-04 01:33:47 +0000

    Start template for bird-clef challenge
    Copy over code from audio classification template

---
+++ pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/full.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+# Common packages are already installed on the compute server
+# Need an additional package? Install it here via:
+#  pip3 install package-name
+
+# Edit the line below to run your experiment (this is just an example). Note:
+#  - This script will be run from your output directory
+#  - Imported Data is accessible via the relative path ../input/
+
+{%- if cookiecutter.data_subset_percentage == '100' %}
+
+/usr/bin/time -f "Time taken: %E" python3 ../code/train.py --epochs 60
+{%- else %}
+
+/usr/bin/time -f "Time taken: %E" python3 ../code/train.py --epochs 60 --subset {{ cookiecutter.data_subset_percentage }}
+{%- endif %}
