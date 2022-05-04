54d418901644f4e11afa29e9420ae217df33e500
Anil Thomas <anlthms@users.noreply.github.com>
2022-05-04 18:42:55 +0000

    Customize template for bird-clef challenge

--- pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/dataset.py
+++ pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/dataset.py
@@ -3,15 +3,18 @@ import cv2
 import librosa
 import numpy as np
 import torch.utils.data as data
+import warnings
+warnings.simplefilter("ignore")
 
 
 class AudioDataset(data.Dataset):
     def __init__(
             self, df, conf, input_dir, imgs_dir,
-            class_names, audio_transform, image_transform, subset=100):
+            class_names, audio_transform, image_transform, subset=100, is_test=False):
         self.conf = conf
         self.audio_transform = audio_transform
         self.image_transform = image_transform
+        self.is_test = is_test
 
         if subset != 100:
             assert subset < 100
@@ -19,12 +22,12 @@ class AudioDataset(data.Dataset):
             num_rows = df.shape[0]*subset//100
             df = df.iloc[:num_rows]
 
-        files = df['{{ cookiecutter.file_column }}']
+        files = df['files']
         assert isinstance(files[0], str), (
             f'column {df.columns[0]} must be of type str')
         self.files = [os.path.join(input_dir, imgs_dir, f) for f in files]
 
-        labels = df['{{ cookiecutter.label_column }}']
+        labels = df['labels']
         num_samples = len(files)
         num_classes = len(class_names)
         class_map = {class_names[i]: i for i in range(num_classes)}
@@ -35,12 +38,22 @@ class AudioDataset(data.Dataset):
 
     def __getitem__(self, index):
         conf = self.conf
-        filename = self.files[index]
+        if self.is_test:
+            filename = self.files[index//12]
+            label = self.labels[index//12]
+            clip_idx = index%12
+            offset = clip_idx*5
+            duration = 5
+        else:
+            filename = self.files[index]
+            label = self.labels[index]
+            offset = conf.offset
+            duration = conf.duration
         assert os.path.isfile(filename)
 
         num_samples =  conf.duration*conf.sample_rate
         sound, rate = librosa.load(
-            filename, sr=conf.sample_rate, offset=conf.offset, duration=conf.duration)
+            filename, sr=conf.sample_rate, offset=offset, duration=duration)
         while (sound.shape[0] < num_samples):
             # pad to required length by duplicating data
             sound = np.hstack((sound, sound[:(num_samples - sound.shape[0])]))
@@ -60,11 +73,14 @@ class AudioDataset(data.Dataset):
         img = cv2.resize(
             img, (conf.spectrogram_width, conf.num_mels),
             interpolation=cv2.INTER_AREA)
+
         img = np.stack((img, img, img), axis=2)
         if self.image_transform:
             img = self.image_transform(image=img)['image']
-        label = self.labels[index]
         return img, label
 
     def __len__(self):
+        if self.is_test:
+            # there are 12 five-second clips in each soundscape
+            return 12*len(self.files)
         return len(self.files)
