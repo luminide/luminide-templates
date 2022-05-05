2d77cf234f7255512b3eb20148c8b812be557ec9
Anil Thomas <anlthms@users.noreply.github.com>
2022-05-05 18:16:38 +0000

    Use focal loss in bird-clef template

--- pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/util.py
+++ pytorch-kaggle-bird-clef/{{cookiecutter and 'code'}}/util.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+import torch
+from torch import nn
 
 class LossHistory:
     def __init__(self):
@@ -25,6 +27,20 @@ class LossHistory:
             'train_loss', 'val_loss', 'epoch_val_loss']
         df = pd.DataFrame(self.data, columns=columns)
         df.to_csv('history.csv', index=False)
+
+class BCEFocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+
+    def forward(self, outputs, targets):
+        bce_loss = nn.BCEWithLogitsLoss(reduction='none')(outputs, targets)
+        probs = torch.sigmoid(outputs)
+        loss = (
+            targets*self.alpha*((1. - probs)**self.gamma)*bce_loss + (
+            (1. - targets)*(probs**self.gamma)*bce_loss))
+        return loss.mean()
 
 def get_class_names(df):
     labels = df['labels']
